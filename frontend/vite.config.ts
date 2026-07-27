1d4f307e790085b508ece732458ee3455eedbb2a
DEBASIS <185465407+DebaA17@users.noreply.github.com>
2026-07-27 12:19:25 +0530

    ci: set es2022 target for dev server and dep optimization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,4 +22,12 @@ export default defineConfig({
   build: {
     target: 'es2022',
   },
+  esbuild: {
+    target: 'es2022',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022',
+    },
+  },
 });
